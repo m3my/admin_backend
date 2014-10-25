069c6767e02398f27d04f4f2d8c6d6ed2f228831
Daniel Karch <github@danielkarch.net>
2014-10-25 20:43:41 +0200

    do not allow tags that are similar to the movie title

--- save_movies_firebase.py
+++ save_movies_firebase.py
@@ -2,27 +2,39 @@ from firebase import firebase
 import json, pickle
 
 
-f = open('imdb_movies.pickle','r')
-imdb_movies = pickle.load(f)
-f.close()
+def filter_tags(tagjson, movietitle):
+  
+  moviewords = set(movietitle.lower().split())
+  alltags = [tag['term'] for tag in tagjson['tags']]
+  return filter(lambda tag: len(moviewords.intersection(tag.lower().split())) == 0, alltags)
 
-f = open('neofonie_movies.pickle','r')
-neofonie_movies = pickle.load(f)
-f.close()
+def main():
 
-mm_movies = []
+  f = open('imdb_movies.pickle','r')
+  imdb_movies = pickle.load(f)
+  f.close()
 
-for i in imdb_movies:
-  mm_movie = {}
-  neofonie_movie = neofonie_movies[i.getID()]
-  mm_movie['Title'] = i['title']
-  mm_movie['Tags'] = [tag['term'] for tag in neofonie_movie['tags']]
-  mm_movie['Cover_Url'] = i['full-size cover url']
-  mm_movie['IMDB_Id'] = i.getID()
-  mm_movies.append(mm_movie)
+  f = open('neofonie_movies.pickle','r')
+  neofonie_movies = pickle.load(f)
+  f.close()
 
-#write to firebase
-firebase = firebase.FirebaseApplication('https://popping-heat-9121.firebaseio.com/', None)
-for m in mm_movies:
-  result = firebase.post('/movies', m)
-  print result
+  mm_movies = []
+  
+  for i in imdb_movies:
+    mm_movie = {}
+    neofonie_movie = neofonie_movies[i.getID()]
+    mm_movie['Title'] = i['title']
+    mm_movie['Tags'] = [tag['term'] for tag in neofonie_movie['tags']]
+    mm_movie['Cover_Url'] = i['full-size cover url']
+    mm_movie['IMDB_Id'] = i.getID()
+    mm_movies.append(mm_movie)
+    
+    #write to firebase
+    firebase = firebase.FirebaseApplication('https://popping-heat-9121.firebaseio.com/', None)
+    for m in mm_movies:
+      result = firebase.post('/movies', m)
+      print result
+      
+      
+if __name__ == "__main__":
+  main()
